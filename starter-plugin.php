bcd97f4b780a287bbeb197da254f2612bc2a9088
sajjadcodes <sakraan01@gmail.com>
2022-05-16 17:07:19 +0500

    added the main file of the plugin

---
+++ starter-plugin.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Plugin Name:       Starter Plugin
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       A WordPress Plugin developement starter. Which helps to build plugin instead of reinventing the basic structure.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Sajad Hussain
+ * Author URI:        https://sajjadcodes.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       starter-plugin
+ * Domain Path:       /languages
+ */
+
+/**
+ * exit if file is directly access.
+ */ 
+
+ if( ! defined('ABSPATH')) exit;
+
+/**
+ *  includes required files
+ */
+
+
+ if( file_exists( dirname(__FILE__) . '/vendor/autoload.php') ) {
+        require_once dirname(__FILE__) . '/vendor/autoload.php';
+ }
+
+
+ use Inc\Init;
+ use Inc\Base\Activate;
+ use Inc\Base\Deactivate;
+
+
+// require_once plugin_dir_path( __FILE__ ).'includes/Init.php';
+// require_once plugin_dir_path( __FILE__ ) . 'includes/Base/Activate.php';
+// require_once plugin_dir_path( __FILE__ ) . 'includes/Base/Deactivate.php';
+
+/**
+ * This function will run during plugin activation
+ */  
+function activate_starter_plugin() {
+   Activate::activate();
+}
+register_activation_hook(__FILE__, 'activate_starter_plugin');
+
+/**
+ * This function will run during plugin deactivation
+ */
+function deactivation_starter_plugin() {
+    Deactivate::deactivate();
+}
+register_deactivation_hook(__FILE__,'deactivation_starter_plugin');
+
+/**
+ * Initialize all the core classes and register hooks
+ */
+
+if ( class_exists('Init') ) {
+
+    Init::getIncludes();
+    Init::registerServices();
+
+}
